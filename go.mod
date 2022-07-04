245f92495e9ffffb9689e58350c1c946523d89ba
faruan <filipe.cezar@gmail.com>
2022-07-04 20:53:24 -0300

    #Aula1-ImersãoFullCycle

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/farua/fullcycle-simulator-go
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import route2 "github.com/faruan/application/route"
+
+func main() {
+	route := route2.Route{
+		ID:			"1",
+		ClientID:	"1",
+	}
+	route.LoadPositions()
+	stringjson, _ := route.ExportJsonPositions()
+	fmt.Println(stringjson[0])
+}
